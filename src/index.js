a0efccf314db7a48075fa13e8e283742b673a824
James Wu <james.wu@airbnb.com>
2020-05-14 15:37:57 -0700

    add typescript

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-
-console.log('hello world');
-console.log(process.env.TEST_SECRET);
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+import ApolloServer from 'apollo-server-express';
+
+const app = express();
+
+const schema = null;
+const resolvers = null;
+
+const x: number = 1;
+console.log(x);
+
